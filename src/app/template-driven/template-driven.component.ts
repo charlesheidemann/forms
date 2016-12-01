ec1677910062c146cdeb1e325edeaa35374340aa
Charles Heidemann <charles.heidemann@gmail.com>
2016-12-01 19:18:01 -0200

    validators

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'ngf-template-driven',
   templateUrl: './template-driven.component.html',
-  styles: []
+  styles: [`
+
+.submitted .ng-invalid{
+    border: 1px solid red;
+}
+  `
+  ]
 })
 export class TemplateDrivenComponent implements OnInit {
+
+  user = {
+    username: '',
+    email: '',
+    password: '',
+    gender: 'male'
+
+  };
+
+  genders = [
+    'male',
+    'female'
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    console.log(this.user['username']);
+    console.log(this.user['email']);
+    console.log(this.user['password']);
   }
 
 }
