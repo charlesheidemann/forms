3c7ecfc205f5b918bdc422b51f20f10725efae9c
Charles Heidemann <charles.heidemann@gmail.com>
2016-12-02 16:16:22 -0200

    Data formGroupControl

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
+//ReactiveFormsModule 113
 
 @Component({
   selector: 'ngf-data-driven',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataDrivenComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+
+  constructor() {
+    this.myForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl('Charles', Validators.required),
+        'email': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
+      }),
+      'password': new FormControl()
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.myForm);
+
   }
 
 }
